18b8d07080c38f3eea2315c06b78ec7f414b994f
Jackie Hobson <j@ckie.xyz>
2023-01-26 16:12:57 -0500

    updated install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,9 @@ touch ~/.zmisc/lastupdated.dat
 # installs ~/.zmisc to $PATH and sets the updater script to run
 # and adds the macos software update/upgrade aliases
 
+echo "" >> ~/.zprofile
+echo "# macscripts settings" >> ~/.zprofile
+echo "" >> ~/.zprofile
 echo 'PATH=$PATH":~/.zmisc"' >> ~/.zprofile
 echo "alias update='softwareupdate -l'" >> ~/.zprofile
 echo "alias upgrade='sudo softwareupdate -iar --agree-to-license'" >> ~/.zprofile
