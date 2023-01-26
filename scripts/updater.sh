6ebdf4ef166bb72a7b8ed64c131fb2307cdf9dfb
Jackie Hobson <j@ckie.xyz>
2023-01-26 15:26:12 -0500

    updated updater.sh

--- scripts/updater.sh
+++ scripts/updater.sh
@@ -4,13 +4,37 @@
 
 if test "`find ~/.zmisc/lastupdated.dat -mmin +30`"; then
 
-# if it is, update homebrew and check for a macos software update
+	# check if there's internet
+	touch ~/.zmisc/checkinternet.dat
+
+	while true; do
+
+		# check if we can ping 9.9.9.9, if so continue the script	
+
+		if $( (( $(ping -c 1 9.9.9.9 > /dev/null ; echo $?) < 1 )) ); then
+			break
+		fi
+
+		# otherwise, if we can't ping 9.9.9.9 then try for 5 minutes then quit
+	
+		if test "`find ~/.zmisc/checkinternet.dat -mmin +5`"; then
+			echo "No internet in the past 5 minutes."
+			exit 1
+		fi
+
+	done
+
+	# delete the file so next time it doesn't just quit
+
+	rm ~/.zmisc/checkinternet.dat
+
+	# update homebrew and check for a macos software update
 
 	brew update -v 
 	brew upgrade -v
 	softwareupdate -l
 
-# touch the file so the next time the script is run the last update is logged
+	# touch the file so the next time the script is run the last update is logged
 
 	touch ~/.zmisc/lastupdated.dat
 fi
