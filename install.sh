3dbc651d0afc56bd940c6c047d59ac5c86068734
Jackie Hobson <j at ckie dot xyz>
2023-01-26 17:08:34 -0500

    updated install.sh

--- install.sh
+++ install.sh
@@ -16,4 +16,5 @@ echo "" >> ~/.zprofile
 echo 'PATH=$PATH":~/.zmisc"' >> ~/.zprofile
 echo "alias update='softwareupdate -l'" >> ~/.zprofile
 echo "alias upgrade='sudo softwareupdate -iar --agree-to-license'" >> ~/.zprofile
+echo "ICLOUD=~/Library/Mobile\ Documents/com\~apple\~CloudDocs" >> ~/.zprofile
 echo "~/.zmisc/updater.sh" >> ~/.zprofile
