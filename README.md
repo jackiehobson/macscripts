743e4b140eb8cca8e5c592aae8d991bc4c7a9b1a
Jackie Hobson <j@ckie.xyz>
2023-01-31 23:06:05 -0500

    updated README.md, added configuration options for install.sh, added .gitignore, added homebrew-less updater script

--- README.md
+++ README.md
@@ -4,8 +4,22 @@ Scripts to enhance MacOS. It autoupdates homebrew and checks for macos updates (
 
 ## Dependencies
 
-Stock MacOS (as of Ventura) includes the required dependencies which are zsh and bash.
+Stock MacOS (as of Ventura) includes the required dependencies which are zsh and bash. 
 
 ## Install
 
-Clone the repo and run install.sh from the git directory.
+### First, there are a few options you should consider.
+
+#### 1. Backing up .ssh, .gnupg, and .config to iCloud
+
+If you want this option, then do `touch ICLOUD.dat` in the git repo. It will automatically back up those directories to iCloud. 
+
+#### 2. Adding homebrew support
+
+If you want to automatically update homebrew, then do `touch BREW.dat` in the git repo. It will make it so the script automatically updates homebrew.
+
+After configurating, run `install.sh` from the git directory.
+
+## Devs
+
+Pull requests welcome, *.dat gitignored. Bash is used only just cause I'm more familiar with it than zsh. Plus syntax won't change at all since bash is frozen in MacOS due to licensing issues (see anti-tivolization).
--- install.sh
+++ install.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+#    install.sh - installs updater.sh or updater_nh.sh and arranges dotfiles
+#    Copyright (C) 2023 Jackie Hobson
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 # ~/.zmisc holds my scripts and misc files
 
 mkdir ~/.zmisc
@@ -18,19 +35,35 @@ echo "" >> ~/.zprofile
 echo 'PATH=$PATH":~/.zmisc"' >> ~/.zprofile
 echo "alias update='softwareupdate -l'" >> ~/.zprofile
 echo "alias upgrade='sudo softwareupdate -iar --agree-to-license'" >> ~/.zprofile
+
+# adds iCloud directory variable 
+
 echo "ICLOUD=~/Library/Mobile\ Documents/com\~apple\~CloudDocs" >> ~/.zprofile
-echo "~/.zmisc/updater.sh" >> ~/.zprofile
+
+# if homebrew support is enabled then add homebrew supported script 
+
+if test -f ./BREW.dat; then
+	echo "~/.zmisc/updater.sh" >> ~/.zprofile
+else
+	echo ~/.zmisc/updater_nh.sh" >> ~/.zprofile
+fi
 
 ICLOUD=~/Library/Mobile\ Documents/com\~apple\~CloudDocs
 
 
 
 # makes symlinks of .ssh, .gnupg, .config from iCloud storage
-# but backs them up first
+# but backs them up first and only if DOTFILES.dat exists in the repo
 
-mkdir ~/.dotfiles.old
-mv ~/.ssh ~/.config ~/.gnupg ~/.dotfiles.old
+if test -f ./ICLOUD.dat; then
 
-ln -s $ICLOUD/dotfiles/ssh ~/.ssh
-ln -s $ICLOUD/dotfiles/config ~/.config
-ln -s $ICLOUD/dotfiles/gnupg ~/.gnupg
+	echo "Copying .ssh, .gnupg, and .config to ~/.dotfiles.old..."
+	mkdir ~/.dotfiles.old
+	mv ~/.ssh ~/.config ~/.gnupg ~/.dotfiles.old
+
+	echo "Making symlinks of .ssh, .gnupg and .config to icloud..."
+	ln -s $ICLOUD/dotfiles/ssh ~/.ssh
+	ln -s $ICLOUD/dotfiles/config ~/.config
+	ln -s $ICLOUD/dotfiles/gnupg ~/.gnupg
+
+fi
