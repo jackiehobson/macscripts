8e89c805de5a6a0a9f436a61341957bcdb25ad8d
Jackie Hobson <j@ckie.xyz>
2023-02-25 13:18:07 -0800

    Revised updater script comments, changed ping to 1.1.1.1

--- scripts/updater.sh
+++ scripts/updater.sh
@@ -16,12 +16,17 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# make it so ctrl-c exits the program and resets the update timer
+# Make sure lock file exists so multiple updates can't run at the same time
+
 LOCKFILE=~/.zmisc/updatelock.dat
 touch $LOCKFILE
+
+# Color codes for error messages
+
 RED='\033[0;31m'
 NOCOLOR='\033[0m'
 
+# Make it so ctrl-c exits the program and resets the update timer
 
 stty -echoctl
 trap ctrl_c INT
@@ -35,19 +40,21 @@ function ctrl_c() {
 	exit 0
 }
 
+# Check if there's an internet connection
+
 function inet() {
-    # check if there's internet
-	touch ~/.zmisc/checkinternet.dat
+
+    touch ~/.zmisc/checkinternet.dat
 
 	while true; do
 
-		# check if we can ping 9.9.9.9, if so continue the script
+		# Check if we can ping 1.1.1.1, if so continue the script
 
-		if $( (( $(ping -c 1 9.9.9.9 > /dev/null ; echo $?) < 1 )) ); then
+		if $( (( $(ping -c 1 1.1.1.1 > /dev/null ; echo $?) < 1 )) ); then
 			break
 		fi
 
-		# otherwise, if we can't ping 9.9.9.9 then try for 5 minutes then quit
+		# Otherwise, if we can't ping 1.1.1.1 then try for 5 minutes then quit
 
 		if test "`find ~/.zmisc/checkinternet.dat -mmin +5`"; then
 			echo "No internet in the past 5 minutes."
@@ -56,13 +63,13 @@ function inet() {
 
 	done
 
-	# delete the file so next time it doesn't just quit
+	# Delete the file so next time it doesn't just quit
 
 	rm ~/.zmisc/checkinternet.dat
 
 }
 
-# check if updates are running already
+# Check if updates are running already
 
 if grep -q 1 "$LOCKFILE"; then
 
@@ -71,30 +78,31 @@ if grep -q 1 "$LOCKFILE"; then
     exit 0
 fi
 
-# check if this file's last modification is older than 30 minutes
+# Check if this file's last modification is older than 30 minutes
 
 if test "`find ~/.zmisc/lastupdated.dat -mmin +30`"; then
 
-    # lock updates to eliminate simultaneous updates
+    # Lock updates to eliminate simultaneous updates
 
     echo "1" > $LOCKFILE
 
-    # check for internet
+    # Check for internet
 
     inet
 
-	# update homebrew and check for a macos software update
+	# Update homebrew and check for a MacOS software update
 
 	brew update -v
 	brew upgrade -v
-	echo
+	# Add an echo so the output looks cleaner
+    echo
 	softwareupdate -l
 
-    # remove the update lock
+    # Remove the update lock
 
     echo "0" > $LOCKFILE
 
-	# touch the file so the next time the script is run the last update is logged
+	# Touch the file so the next time the script is run the last update is logged
 
 	touch ~/.zmisc/lastupdated.dat
 fi
